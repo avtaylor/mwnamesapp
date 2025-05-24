84fa2159c93fd2e9a56199e7ccfa31acbaff70f5
avtaylor <avtaylor14@gmail.com>
2025-05-24 15:19:37 +0200

    Update app.R

--- app.R
+++ app.R
@@ -48,7 +48,7 @@ if (httr::status_code(res) != 200) {
 download_url <- httr::content(res)$download_url
 
 # Read CSV directly
-df <- read.csv(download_url, stringsAsFactors = FALSE,check.names = FALSE))
+df <- read.csv(download_url, stringsAsFactors = FALSE,check.names = FALSE)
 colnames(df) <- tolower(trimws(colnames(df)))
 if (!all(c("name", "frequency", "district") %in% colnames(df))) {
     stop("CSV must contain 'name', 'frequency', and 'district' columns.",head(df, 10))
